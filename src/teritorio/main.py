d59057dde500917609313f48c236e386417debf7
Stephanos Kuma <stephanos@kuma.ai>
2024-09-06 16:47:09 +0100

    ⏫ Update dependencies

--- src/teritorio/main.py
+++ src/teritorio/main.py
@@ -7,29 +7,29 @@ from typing import Any, Generic, TypeVar
 
 from pyutilkit.classes import Singleton
 
-T = TypeVar("T")
+_T = TypeVar("_T")
 
 
-class DataListIterator(Generic[T]):
-    def __init__(self, data: dict[str, T]) -> None:
+class DataListIterator(Generic[_T]):
+    def __init__(self, data: dict[str, _T]) -> None:
         self.values = (data[key] for key in sorted(data))
 
-    def __iter__(self) -> DataListIterator[T]:
+    def __iter__(self) -> DataListIterator[_T]:
         return self
 
-    def __next__(self) -> T:
+    def __next__(self) -> _T:
         return next(self.values)
 
 
-class DataList(Generic[T]):
+class DataList(Generic[_T]):
     _data_path: Path
-    _object_class: type[T]
+    _object_class: type[_T]
 
     def __init__(self) -> None:
         with self._data_path.open() as file:
             data = json.load(file, object_hook=_list_to_tuple)
 
-        self._data: dict[str, T] = {}
+        self._data: dict[str, _T] = {}
         for key, raw_obj in data.items():
             obj = self._object_class(**raw_obj)
             self._data[key] = obj
@@ -44,10 +44,10 @@ class DataList(Generic[T]):
     def __len__(self) -> int:
         return len(self._data)
 
-    def __getitem__(self, key: str) -> T:
+    def __getitem__(self, key: str) -> _T:
         return self._data[key]
 
-    def __iter__(self) -> DataListIterator[T]:
+    def __iter__(self) -> DataListIterator[_T]:
         return DataListIterator(self._data)
 
 
